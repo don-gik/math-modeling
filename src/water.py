c479506982cabb877ce72c1cad4b52f6f560cbc3
don-gik <don-gik@github.com>
2025-11-27 10:33:30 +0900

    Path error hotfix

--- src/water.py
+++ src/water.py
@@ -1,8 +1,10 @@
 import pandas as pd
+import os
 
 from omegaconf import DictConfig, ListConfig
 
 def main(config: DictConfig | ListConfig):
     df = pd.read_csv(getattr(config, "water_use"))
     df = df["Water Usage(m^3)"].str.replace(',', '').astype(float)
+    os.makedirs("data/processed", exist_ok=True)
     df.to_json("data/processed/water_use.json", indent=4)
