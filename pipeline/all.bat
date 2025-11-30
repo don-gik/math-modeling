e0a1f5b6b00fea66784b7fc5d0950bf3eed02a7f
don-gik <don-gik@github.com>
2025-11-30 18:05:23 +0900

    Plotted

--- pipeline/all.bat
+++ pipeline/all.bat
@@ -3,3 +3,5 @@ call "pipeline/data-process.bat"
 call "pipeline/simulate-season.bat"
 
 call "pipeline/simulate-sine.bat"
+
+call "pipeline/predict-season.bat"
---
+++ pipeline/predict-season.bat
@@ -0,0 +1,1 @@
+uv run main.py --config-name predict-season.yaml
